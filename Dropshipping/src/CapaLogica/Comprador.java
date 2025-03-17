0e88e254fd245782b2a14c1d88bd180db10dcbdd
xTavoM <andresmoreraquiros@gmail.com>
2025-03-16 23:36:21 -0600

    Update Comprador.java

--- Dropshipping/src/CapaLogica/Comprador.java
+++ Dropshipping/src/CapaLogica/Comprador.java
@@ -1,10 +1,17 @@
 package CapaLogica;
 
+/**
+  Representa un comprador en la tienda.
+  Hereda de Usuario e implementa el método mostrarInformacion().
+ **/
 public class Comprador extends Usuario {
     public Comprador(String nombreUsuario, String nombreCompleto, String cedulaIdentidad, String fechaNacimiento, String correoElectronico, String password) {
         super(nombreUsuario, nombreCompleto, cedulaIdentidad, fechaNacimiento, correoElectronico, password);
     }
 
+    /**
+      Muestra información del comprador.
+    **/
     @Override
     public void mostrarInformacion() {
         System.out.println("Comprador: " + nombreCompleto + " - " + correoElectronico);
