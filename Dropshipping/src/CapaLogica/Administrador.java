98a465876b228ac93cf6a9fe4b0269fdc8d8e0a0
xTavoM <andresmoreraquiros@gmail.com>
2025-03-16 23:34:37 -0600

    Update Administrador.java

--- Dropshipping/src/CapaLogica/Administrador.java
+++ Dropshipping/src/CapaLogica/Administrador.java
@@ -1,10 +1,17 @@
 package CapaLogica;
 
+/**
+  Representa un administrador del sistema.
+  Hereda de Usuario e implementa el método mostrarInformacion().
+ **/
 public class Administrador extends Usuario {
     public Administrador(String nombreUsuario, String nombreCompleto, String cedulaIdentidad, String fechaNacimiento, String correoElectronico, String password) {
         super(nombreUsuario, nombreCompleto, cedulaIdentidad, fechaNacimiento, correoElectronico, password);
     }
 
+     /**
+      Muestra información del administrador.
+     **/
     @Override
     public void mostrarInformacion() {
         System.out.println("Administrador: " + nombreCompleto);
